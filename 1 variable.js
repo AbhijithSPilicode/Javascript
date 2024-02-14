861d58de77b18d165b02439bc446089d5f1be762
Abhijith S <93380590+AbhijithSPilicode@users.noreply.github.com>
2024-02-14 16:35:53 +0530

    basics of variable

---
+++ 1 variable.js
@@ -0,0 +1,14 @@
+var age=21;  //number
+var FirstName="messi";  //string
+var online=true;  //boolean
+var whatever; //it will show undefined since not given any value
+var variable=null; //null means there is no value assigned to this variable
+age=age+1;
+console.log("Your age is",age);
+console.log("he is",FirstName);
+console.log("Are you online",online)
+console.log(whatever)
+console.log(variable)
+let age1=21; //let is used instead of var to store value temporarilly
+const age2=23; //const value cannot be changed.it's fixed.
+//so after that you cannot assign age2=some other number
