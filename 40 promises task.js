e44867001737fc440d82cf225f9fbb97b2f4ce83
Abhijith S <93380590+AbhijithSPilicode@users.noreply.github.com>
2024-03-26 17:51:48 +0530

    Add files via upload

---
+++ 40 promises task.js
@@ -0,0 +1,26 @@
+async function getData() {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await response.json();
+      console.log(data);
+      var player='<h2>Lists of Users</h2>';
+      data.forEach(function(user) {
+           player+=`<div class="player">
+                      <div class="strength">Name : ${user.name}</div>
+                      <div>Email   : ${user.email}</div>
+                      <div>Phone   : ${user.phone}</div>
+                      <div>Website : ${user.website}</div>
+                      <div>Company : ${user.company.name}</div>
+                      <div>City    : ${user.address.city}</div>
+                      <div>Zipcode : ${user.address.zipcode}</div>
+                     </div>`;
+})
+    document.getElementById("message").innerHTML=player
+    } catch (error) {
+      console.log("Promise rejected.");
+      console.log(error.message)
+    }
+  }
+  let button=document.getElementById("btnGet")
+  button.addEventListener('click',getData);
+
